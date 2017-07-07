4e0bb6dcfbc82455832751333ebfcba92a954279
Ihsan Helmi Faisal <ihsan.jtk09@gmail.com>
2017-07-07 08:50:42 +0700

    init

---
+++ app/src/main/java/id/ihsan/popmovie/MovieApplication.java
@@ -0,0 +1,34 @@
+package id.ihsan.popmovie;
+
+import android.app.Application;
+
+/**
+ * Created by Ihsan Helmi Faisal
+ * on 7/6/2017.
+ */
+
+public class MovieApplication extends Application {
+
+    private static MovieApplication instance;
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static synchronized MovieApplication getInstance() {
+        return instance;
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
