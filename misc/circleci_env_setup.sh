69bd08f9e7698b66c51effd7e0e97589d867f7b3
Ihsan Helmi Faisal <ihsan.jtk09@gmail.com>
2018-09-10 03:43:02 +0700

    Fabric properties

--- misc/circleci_env_setup.sh
+++ misc/circleci_env_setup.sh
@@ -5,15 +5,26 @@
 # 3. https://stackoverflow.com/questions/35440907/can-circle-ci-reference-gradle-properties-credentials
 
 export GRADLE_PROPERTIES=$HOME"/popmovie/gradle.properties"
+export FABRIC_PROPERTIES=$HOME"/popmovie/app/fabric.properties"
 
 function copyEnvVarsToProperties {
 
     echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+    echo "Fabric Properties should exist at $FABRIC_PROPERTIES"
+
+    if [ ! -f "$FABRIC_PROPERTIES" ]
+    then
+        echo "${FABRIC_PROPERTIES} does not exist...Creating file"
+
+        touch ${FABRIC_PROPERTIES}
+
+        echo "apiKey=$FABRIC_API_KEY" >> ${FABRIC_PROPERTIES}
+        echo "apiSecret=$FABRIC_API_SECRET" >> ${FABRIC_PROPERTIES}
+    fi
 
     if [ ! -f "$GRADLE_PROPERTIES" ]
     then
         echo "${GRADLE_PROPERTIES} does not exist...Creating Properties file"
-        echo "FABRIC_API_KEY=$FABRIC_API_KEY" >> ${GRADLE_PROPERTIES}
     fi
 }
 
