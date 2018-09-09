26ef13cd61d473790930466c5ced302b08b2bba1
Ihsan Helmi Faisal <ihsan.jtk09@gmail.com>
2018-09-10 02:27:14 +0700

    Install env setup

---
+++ misc/circleci_env_setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# 1. http://deathstartup.com/?p=81
+# 2. https://gist.github.com/KioKrofovitch/716e6a681acb33859d16
+# 3. https://stackoverflow.com/questions/35440907/can-circle-ci-reference-gradle-properties-credentials
+
+export GRADLE_PROPERTIES=$HOME"/popmovie/gradle.properties"
+
+function copyEnvVarsToProperties {
+
+    echo "Gradle Properties should exist at $GRADLE_PROPERTIES"
+
+    if [ ! -f "$GRADLE_PROPERTIES" ]
+    then
+        echo "${GRADLE_PROPERTIES} does not exist...Creating Properties file"
+
+	echo "FABRIC_API_KEY=FABRIC_API_KEY" >> ${GRADLE_PROPERTIES}
+
+    fi
+}
+
+# execute functions
+copyEnvVarsToProperties
